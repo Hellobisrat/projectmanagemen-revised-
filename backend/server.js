f2a6a86f0bd4f0871e63ec708a94d34124e3b0dc
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-04-08 10:43:56 -0500

    serverjs added on backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import userRouter from './route/useRoute.js';
+import dbConnect from './config/db.js';
+import taskRouter from './route/taskRoute.js';
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json())
+app.use(cors())
+app.use('/api/user',userRouter);
+app.use('/api/tasks',taskRouter)
+app.get('/',(req,res)=>{
+  res.json('well come to Home')
+})
+app.listenerCount(PORT,()=>{
+  dbConnect();
+  console.log(`app start listening at http://localhost:${PORT}`)
+})
